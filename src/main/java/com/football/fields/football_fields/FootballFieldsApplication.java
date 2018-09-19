babbcd01cf7474a3b614b553145d7f18b3be04df
Eridan123 <saryguloveridan@gmail.com>
2018-09-19 15:54:55 +0600

    start

---
+++ src/main/java/com/football/fields/football_fields/FootballFieldsApplication.java
@@ -0,0 +1,12 @@
+package com.football.fields.football_fields;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballFieldsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballFieldsApplication.class, args);
+    }
+}
